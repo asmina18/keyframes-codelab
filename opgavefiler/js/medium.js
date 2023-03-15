257d1cb328e0ba87f4f35847b3c10db124586dee
Amina <113165708+asmina18@users.noreply.github.com>
2023-03-15 14:46:28 +0100

    færdig med opgave /easy.css/medium.css

--- opgavefiler/js/medium.js
+++ opgavefiler/js/medium.js
@@ -3,6 +3,9 @@
 let myBall=document.getElementById('ballOne');
 let myBuhButton=document.getElementById('buhOneButton');
 
+myBuhButton.addEventListener('click',()=>{
+    myBall.classList.toggle('animateRight');
+})
 
 
 
@@ -11,12 +14,40 @@ let myBuhButton=document.getElementById('buhOneButton');
 let myBalls=document.getElementsByClassName('balls');
 let myBuhTwoButton=document.getElementById('buhTwoButton');
 
+myBuhTwoButton.addEventListener('click',()=>{
+    for(const manyBolls of myBalls){
+        manyBolls.classList.toggle('animateRight');
+       }
+});
+ 
+
 
 
 /*opgave 3*/
 
 
 
+let myHardBalls=document.getElementsByClassName('hardBalls');
+let myBuhThreeButton =document.getElementById('buhThreeButton');
+
+myBuhThreeButton.addEventListener('click',()=>{
+    let direction = "right";
+
+    for(const manyBolls of myHardBalls){
+
+        if (direction =="right"){
+            manyBolls.classList.toggle('animateRight');
+            direction="left";
+        }
+
+        
+        else{
+            manyBolls.classList.toggle('animateLeft');
+            direction= "right";
+        }
+       }
+       
+});
 
 
 
